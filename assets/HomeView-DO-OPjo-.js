cb1c9042aa2c986fd55ccc1c9201a25e3c9af3b9
sima <simagyym2003@gmail.com>
2024-02-29 00:02:36 +0300

    adding dist

---
+++ assets/HomeView-DO-OPjo-.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c,a as t}from"./index-DDLJWMOP.js";const n={},s=t("h1",null,"Главная страница",-1),a=[s];function _(r,l){return c(),o("main",null,a)}const d=e(n,[["render",_]]);export{d as default};
